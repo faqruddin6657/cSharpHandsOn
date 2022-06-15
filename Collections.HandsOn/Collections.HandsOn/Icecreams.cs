22ba5d0def8d4c5cc9ee258736e30499bb0b3577
shaikhFaqruddin <shaikhfaqru786@gmail.com>
2022-06-15 14:04:56 +0530

    Collections.HandsOn

---
+++ Collections.HandsOn/Collections.HandsOn/Icecreams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.HandsOn
+{
+    internal class Icecreams
+    {
+       public List<string> Icecream = new List<string>();
+        public void AddFlvours()
+        {
+            Console.WriteLine("enter the flavour to be added");
+            string flavour = Console.ReadLine();
+            Icecream.Add(flavour);
+        }
+        public void DisplayFlavours()
+        {
+            foreach(string flavour in Icecream)
+            {
+                Console.WriteLine(flavour); 
+            }
+        }
+        public void RemoveFlavour()
+        {
+            Console.WriteLine("enter the flavour to be removed");
+            string rmflavour = Console.ReadLine();
+            if(Icecream.Contains(rmflavour))
+            {
+                Icecream.Remove(rmflavour);
+            }
+        }
+    }
+}
---
+++ Collections.HandsOn/Collections.HandsOn/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.HandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Icecreams icecreams = new Icecreams();
+            while(true)
+            {
+                Console.WriteLine("for displaying flavours press 1 ");
+                Console.WriteLine("for adding flavours press 2 ");
+                Console.WriteLine("for removing flavours press 3 ");
+                int choice =Convert.ToInt32(Console.ReadLine());
+                if(choice==1)
+                {
+                    icecreams.DisplayFlavours();
+
+                }
+                else if(choice==2)
+                {
+                    icecreams.AddFlvours();
+                }
+                else if(choice==3)
+                {
+                    icecreams.RemoveFlavour();
+                }
+                else
+                {
+                    Console.WriteLine("enter a valid choice");
+                }
+            }
+        }
+    }
+}
