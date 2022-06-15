4d3093198c900b7bc6e56b2b7cea07403832ffa7
shaikhFaqruddin <shaikhfaqru786@gmail.com>
2022-06-15 13:01:54 +0530

    dataAdapter.handson and dataAdapter.practice

---
+++ DataAdapter.HandsOn/DataAdapter.HandsOn/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAdapter.HandsOn
+{
+    internal class Program
+    {
+        public static void Insert()
+        {
+            DataTable dt = new DataTable();
+            Console.WriteLine("enter id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter customer name");
+            string customer =Console.ReadLine();
+            Console.WriteLine("enter email id");
+            string email= Console.ReadLine();
+            Console.WriteLine("enter mobile number");
+            string mob = Console.ReadLine();
+            Console.WriteLine("enter address");
+            string address = Console.ReadLine();
+            string connectionstr = @"Data Source=FAQRUDDIN\SQLEXPRESS;Initial Catalog=OranizationDB;Integrated Security=True";
+            SqlConnection sq = new SqlConnection(connectionstr);
+            SqlDataAdapter adp = new SqlDataAdapter("insert into customer values("+id+",'"+customer+"','"+email+"','"+mob+"','"+address+"')",sq);
+            int res = adp.Fill(dt);
+            if(res>0)
+            {
+                Console.WriteLine("the records are added in the database");
+            }
+            
+        }
+        public static void Delete()
+        {
+            Console.WriteLine("enter the name of the customer to be delted");
+            string name = Console.ReadLine();
+            DataTable dt = new DataTable();
+            string connectionstr = @"Data Source=FAQRUDDIN\SQLEXPRESS;Initial Catalog=OranizationDB;Integrated Security=True";
+            SqlConnection sq = new SqlConnection(connectionstr);
+            SqlDataAdapter adp = new SqlDataAdapter("delete from customer where cusname='"+name+"'", sq);
+            int res = adp.Fill(dt);
+            if (res > 0)
+            {
+                Console.WriteLine("the customer details has been deleted");
+            }
+        }
+
+        public static void Select()
+        {
+            DataTable dt = new DataTable();
+            string connectionstr = @"Data Source=FAQRUDDIN\SQLEXPRESS;Initial Catalog=OranizationDB;Integrated Security=True";
+            SqlConnection sq = new SqlConnection(connectionstr);
+            SqlDataAdapter adp = new SqlDataAdapter("select * from customer", sq);
+            int res = adp.Fill(dt);
+            if (res > 0)
+            {
+                Console.WriteLine("the customer table contains the following records"+"\n");
+            }
+
+           
+
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                for(int j=0;j<dt.Columns.Count;j++)
+                {
+                    Console.Write(dt.Rows[i][j].ToString()+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void UpdateMobile()
+        {
+            
+            Console.WriteLine("enter the name of the customer to be delted");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter the mobile number to be updated");
+            string mobile = Console.ReadLine();
+            DataTable dt = new DataTable();
+            string connectionstr = @"Data Source=FAQRUDDIN\SQLEXPRESS;Initial Catalog=OranizationDB;Integrated Security=True";
+            SqlConnection sq = new SqlConnection(connectionstr);
+            SqlDataAdapter adp = new SqlDataAdapter("update customer set mobile='"+mobile+"' where cusname='"+name+"'", sq);
+            int res = adp.Fill(dt);
+            if (res > 0)
+            {
+                Console.WriteLine("the customer details has been deleted");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //Insert();
+            //UpdateMobile();
+            // Delete();
+            Select();
+
+
+        }
+    }
+}
---
+++ SqlDataAdapter.practice/SqlDataAdapter.practice/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace sqlDataAdapter.practice
+{
+    
+   
+    internal class Program { 
+        public static void withSqldataAdapter()
+        {
+             string constr = @"Data Source=FAQRUDDIN\SQLEXPRESS;Initial Catalog=OranizationDB;Integrated Security=True";
+            try
+            {
+                DataTable dt = new DataTable();
+                SqlConnection sq = new SqlConnection(constr);
+                SqlDataAdapter dadp = new SqlDataAdapter("select * from customer", sq);
+                dadp.Fill(dt);
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        Console.WriteLine(dt.Rows[i][j]);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void withoutAdapter()
+        {
+            DataTable dt = new DataTable();
+            string constr = @"Data Source=FAQRUDDIN\SQLEXPRESS;Initial Catalog=OranizationDB;Integrated Security=True";
+            SqlConnection sq= new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand("select * from customer",sq);
+            sq.Open();
+            SqlDataReader result=cmd.ExecuteReader();
+            dt.Load(result);
+            sq.Close();
+            
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    Console.WriteLine(dt.Rows[i][j]);
+                }
+            }
+
+        }
+
+        public static void Main()
+        {
+            withoutAdapter();
+            //withSqldataAdapter();
+        }
+    
+
+    
+    
+       
+        
+    }
+}
