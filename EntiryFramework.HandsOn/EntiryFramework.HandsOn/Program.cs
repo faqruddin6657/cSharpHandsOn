41fabde19f75e105f74a0601516d33b6dedaf00f
shaikhFaqruddin <shaikhfaqru786@gmail.com>
2022-06-15 13:46:52 +0530

    EntityFramework.practice

---
+++ EntiryFramework.HandsOn/EntiryFramework.HandsOn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntiryFramework.HandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OranizationDBEntities oranizationDBEntities = new OranizationDBEntities();
+            
+            Employee employee = new Employee();
+            employee.eid = Convert.ToInt32(Console.ReadLine());
+            employee.ename =Console.ReadLine();
+            employee.designation =Console.ReadLine();
+            oranizationDBEntities.Employees.Add(employee);
+            oranizationDBEntities.SaveChanges();
+        }
+    }
+}
